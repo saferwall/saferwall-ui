4a7611c4b3876125344e3b329bcd70392867b488
yassinrais <yassmailrais@gmail.com>
2023-01-01 01:23:43 +0000

    init: Saferwall soon reborn in a sveltekit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>Saferwall 2.0</title>
+</svelte:head>
+<main class="flex-1 h-screen w-full flex flex-col items-center justify-center">
+	<img loading="lazy" class="w-48 h-48" src="/images/logo.png" alt="Saferwall logo" />
+	<h1 class="text-3xl">Saferwall soon reborn in a sveltekit</h1>
+</main>
