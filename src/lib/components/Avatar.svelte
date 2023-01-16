f532a9bd6e4fa53041271ff292e3bfeb773824b5
yassinrais <yassmailrais@gmail.com>
2023-01-16 22:29:57 +0000

    feat: add diff size for avatar

--- src/lib/components/Avatar.svelte
+++ src/lib/components/Avatar.svelte
@@ -1,19 +1,24 @@
 <script lang="ts">
-	export let image: string | undefined;
-	export let username: string | undefined;
+	export let size: string = 'base';
+	export let image: string | undefined = undefined;
+	export let username: string | undefined = undefined;
 </script>
 
-<a class="avatar" href="/user/{username}">
+<a class="avatar avatar--{size}" href="/user/{username}">
 	{#if image}
 		<img class="avatar__image" src={image} alt="Avatar of {username}" />
-	{:else}
+	{:else if username}
 		<span class="avatar__text">{username?.slice(0, 3)}</span>
+	{:else}
+		<svg class="w-full h-full text-grayx-100">
+			<use href="/images/images.svg#default-avatar" />
+		</svg>
 	{/if}
 </a>
 
 <style lang="scss">
 	.avatar {
-		@apply w-20 h-20 rounded-full bg-grayx-100 overflow-hidden;
+		@apply rounded-full bg-white overflow-hidden;
 
 		&:has(&__text) {
 			@apply flex items-center justify-center flex-shrink-0 text-2xl font-bold text-grayx-900;
@@ -21,6 +26,14 @@
 
 		&__image {
 			@apply w-full h-full object-cover;
+		}
+
+		&--base {
+			@apply w-20 h-20;
+		}
+
+		&--large {
+			@apply w-28 h-28;
 		}
 	}
 </style>
