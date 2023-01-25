c8b39a41e523e599e04a421ec971554af1fd12e1
yassinrais <yassmailrais@gmail.com>
2023-01-25 23:56:47 +0000

    fix: virtual screen remove max width

--- src/lib/components/cards/summary/VirtualScreensCard.svelte
+++ src/lib/components/cards/summary/VirtualScreensCard.svelte
@@ -5,7 +5,7 @@
 
 <BaseCard>
 	<h1 class="card__title">Virtual Screens</h1>
-	<div class="relative grid md:grid-cols-2 lg:grid-cols-3 gap-10 p-6 pb-12">
+	<div class="relative grid md:grid-cols-2 lg:grid-cols-3 gap-10 pb-12">
 		<div class="virtual__preview" />
 		<div class="virtual__preview hidden md:block" />
 		<div class="virtual__preview hidden lg:block" />
@@ -16,7 +16,7 @@
 <style lang="scss">
 	.virtual {
 		&__preview {
-			@apply w-full h-64 bg-gray-400 border border-gray-200 rounded bg-cover max-w-[400px] mx-auto;
+			@apply w-full h-64 bg-gray-400 border border-gray-200 rounded bg-cover mx-auto;
 		}
 	}
 </style>
