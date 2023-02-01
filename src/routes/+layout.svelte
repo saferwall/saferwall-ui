fb15dd06e447ca1667e4f2cca97d3652be95a44f
yassinrais <yassmailrais@gmail.com>
2023-02-01 23:37:55 +0000

    feat: add progressbar on navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,21 @@
 <script>
 	import '../app.scss';
+
+	import NProgress from 'nprogress';
+	import { navigating } from '$app/stores';
+
+	NProgress.configure({
+		minimum: 0.16
+	});
+
+	$: {
+		if ($navigating) {
+			NProgress.start();
+		}
+		if (!$navigating) {
+			NProgress.done();
+		}
+	}
 </script>
 
 <slot />
