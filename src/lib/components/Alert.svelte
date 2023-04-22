f67fc29a2f549306cdc4fafe071f701d313016ae
yassinrais <yassmailrais@gmail.com>
2023-04-22 14:29:08 +0000

    feat: add alert component

---
+++ src/lib/components/Alert.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	import Icon from './Icon.svelte';
+
+	const dispatch = createEventDispatcher();
+	export let close: boolean = true;
+	export let type: 'error' | 'warning' | 'success' | 'info' = 'info';
+
+	const onClose = () => {
+		dispatch('close');
+	};
+</script>
+
+<div class={`alert ${type}`}>
+	<slot />
+	{#if close}
+		<div class="cursor-pointer" on:mouseup={onClose}>
+			<Icon name="close" />
+		</div>
+	{/if}
+</div>
+
+<style lang="scss">
+	.alert {
+		@apply flex items-center justify-between;
+		@apply border p-4 rounded font-medium;
+
+		&.error {
+			@apply text-red-500 border-red-400;
+		}
+		&.success {
+			@apply text-green-500 border-green-400;
+		}
+		&.info {
+			@apply text-blue-500 border-blue-400;
+		}
+		&.warning {
+			@apply text-orange-500 border-orange-400;
+		}
+	}
+</style>
