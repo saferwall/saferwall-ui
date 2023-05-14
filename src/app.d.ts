e7d727f4bccb9a9ddf4e0e8d60b1455a2ee6b91e
yassinrais <yassmailrais@gmail.com>
2023-05-14 16:54:48 +0000

    fix: app d types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,8 +1,5 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
-
-import type { Session, User } from "$lib/types";
-
 // and what to do when importing types
 declare namespace App {
 	// interface Error {}
