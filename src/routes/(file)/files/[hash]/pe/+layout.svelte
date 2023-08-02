b17b3492ea5bb38b06259411ac2a67a7e8ee0908
Yassine Rais <yassmailrais@gmail.com>
2023-08-02 20:16:42 +0200

    fix: file pe menu

--- src/routes/(file)/files/[hash]/pe/+layout.svelte
+++ src/routes/(file)/files/[hash]/pe/+layout.svelte
@@ -1,18 +1,17 @@
 <script lang="ts">
-	import type { PageData } from './$types';
+	import type { LayoutData } from './$types';
 	import BaseCard from '$lib/components/cards/BaseCard.svelte';
 	import { peMenu } from '$lib/data/menus';
 
-	export let data: PageData;
+	export let data: LayoutData;
 
 	$: activeMenu = data.activePEMenu;
-	$: menus = [...peMenu].filter(({ field }) => data.file.pe?.meta.includes(field));
 </script>
 
 <div class="files__layout container mx-auto w-full h-full items-center justify-center">
 	<BaseCard class="flex flex-col md:flex-row space-x-4" flex={false} padding={false}>
 		<ul>
-			{#each menus as item}
+			{#each peMenu as item}
 				<li><a class:active={item.path === activeMenu} href={item.path}>{item.title}</a></li>
 			{/each}
 		</ul>
--- src/routes/(file)/files/[hash]/pe/+layout.ts
+++ src/routes/(file)/files/[hash]/pe/+layout.ts
@@ -1,8 +1,15 @@
+import type { APIFile } from '$lib/types/files';
 import type { LayoutLoad } from './$types';
 
-export const load = (async ({ parent }) => {
-    const { paths } = await parent();
-    return {
-        activePEMenu: paths[2]
-    };
+export const load = (async ({
+	parent
+}): Promise<{
+	activePEMenu: string;
+	file: APIFile;
+}> => {
+	const { paths, ...parentData } = await parent();
+	return {
+		...parentData,
+		activePEMenu: paths[2]
+	};
 }) satisfies LayoutLoad;
