01be349ed41c8e7cd42f355789744f79a9b68bf1
yassinrais <yassmailrais@gmail.com>
2023-11-25 19:49:06 +0100

    fix: improve multitoggle

--- src/lib/components/Multitoggle.svelte
+++ src/lib/components/Multitoggle.svelte
@@ -1,22 +1,53 @@
 <script lang="ts">
 	import { createEventDispatcher } from 'svelte';
 
-	export let items: string[] = [];
-	export let active: string | undefined = undefined;
+	type MultitoggleItem = {
+		label: string;
+		value: any;
+	} & Partial<{
+		checked: boolean;
+	}>;
+
+	export let max = 1;
+	export let items: MultitoggleItem[] = [];
+	export let name: string | undefined = undefined;
+
+	$: selected = new Set([...items.filter((i) => i.checked)]);
+	$: dispatch('change', selected);
 
 	const dispatch = createEventDispatcher();
 
-	let activeItem = active || items[0] || '';
-	const onToggleMouseUp = (item: string): void => {
-		activeItem = item;
-		dispatch('change', item);
+	const onToggleMouseUp = (target: MultitoggleItem) => {
+		selected = new Set([
+			...items.filter((item) => {
+				const targetItem = item == target;
+				if (max === 1) {
+					return targetItem;
+				}
+
+				if (!targetItem) {
+					return selected.has(item);
+				}
+
+				if (selected.has(item)) {
+					return false;
+				}
+
+				if (selected.size >= max) {
+					return false;
+				}
+
+				return true;
+			})
+		]);
 	};
 </script>
 
 <ul class="multitoggle">
 	{#each items as item}
-		<li on:mouseup={() => onToggleMouseUp(item)} class="item" class:active={activeItem == item}>
-			{item}
+		<li class="item" class:active={selected.has(item)} on:mouseup={() => onToggleMouseUp(item)}>
+			<input type="checkbox" {name} bind:value={item.value} checked={selected.has(item)} />
+			{item.label || item.value}
 		</li>
 	{/each}
 </ul>
@@ -32,6 +63,10 @@
 			&:hover,
 			&.active {
 				@apply bg-primary text-white;
+			}
+
+			input {
+				@apply hidden;
 			}
 		}
 	}
