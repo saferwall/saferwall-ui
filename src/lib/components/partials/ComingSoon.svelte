cd2a4e5a6ef6d76b81a910827a9b80fbc74328c6
yassinrais <yassmailrais@gmail.com>
2023-11-25 03:24:59 +0100

    feat(coming soon): decrease opcty

--- src/lib/components/partials/ComingSoon.svelte
+++ src/lib/components/partials/ComingSoon.svelte
@@ -1,7 +1,7 @@
 <div
-	class="w-full absolute bottom-0 bg-gradient-to-t from-white to-transparent h-full  backdrop-blur-xs"
+	class="w-full absolute bottom-0 bg-gradient-to-t from-white to-transparent h-full opacity-90 backdrop-blur-xs"
 >
 	<div class="flex items-center justify-center w-full h-full bg-white bg-opacity-60">
-		<p class="text-3xl font-bold text-primary ">🚀 Coming soon!</p>
+		<p class="text-3xl font-bold text-primary">🚀 Coming soon!</p>
 	</div>
 </div>
