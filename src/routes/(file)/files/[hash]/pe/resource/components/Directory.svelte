e65eeea917ed991c8a4242711dad8ea7ad9290f2
sickl8 <sicklevy@gmail.com>
2024-08-01 20:39:39 +0100

    almost done fixing resource PE section, need to fix table css

--- src/routes/(file)/files/[hash]/pe/resource/components/Directory.svelte
+++ src/routes/(file)/files/[hash]/pe/resource/components/Directory.svelte
@@ -8,7 +8,10 @@
 </script>
 
 {#if entries}
-	<ul class="space-y-4">
+	<ul class="--space-y-4">
+		<li>
+			<slot/>
+		</li>
 		{#each entries as entry}
 			<li>
 				<DirectoryEntry {...entry} />
@@ -16,5 +19,46 @@
 		{/each}
 	</ul>
 {:else}
-	<ResourceData {struct} />
+	<ul>
+		<li class="resource">
+			<ResourceData {struct} />
+		</li>
+	</ul>
 {/if}
+
+<style lang="postcss">
+	ul li {
+		@apply ml-2;
+		@apply relative;
+		&:after, &:before {
+			@apply content-[''] absolute;
+		}
+		&:after {
+			@apply h-[1px] w-4 top-4 left-0 bg-zinc-300 dark:bg-zinc-700;
+		}
+		&:before {
+			/* @apply border-l h-full; */
+		}
+	}
+	ul li:not(:last-child):before {
+		@apply border-l border-l-zinc-300 dark:border-l-zinc-700 h-full;
+	}
+	ul li:last-child :global(> article > div) {
+		@apply relative;
+		&:before {
+			@apply content-[''] absolute;
+			@apply border-l border-l-zinc-300 dark:border-l-zinc-700 !rounded-none h-full;
+			@apply -top-[calc(0.8rem)] left-0 bg-zinc-300 dark:bg-zinc-700;
+		}
+	}
+	ul li.resource :global(> article > div:first-child) {
+		&:before {
+			@apply h-[50%] top-[calc(0.1rem)];
+		}
+		}
+	ul li.resource :global(> article > div:last-child:not(:first-child)) {
+		&:before {
+			@apply border-none;
+		}
+	}
+</style>
