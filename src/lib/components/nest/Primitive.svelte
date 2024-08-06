cdd697f23ee725fc06790ddf7da2e8ffc69d2ceb
sickl8 <sicklevy@gmail.com>
2024-08-06 01:47:51 +0100

    improved RenderObjectComponent to display array with primitive values

---
+++ src/lib/components/nest/Primitive.svelte
@@ -0,0 +1,9 @@
+<script context="module" lang="ts">
+	export type PrimitiveType = number | string | boolean | null | undefined;
+</script>
+
+<script lang="ts">
+	export let value: PrimitiveType;
+</script>
+
+{value}{#if typeof value === "number"}&nbsp;({"0x" + value.toString(16)}){/if}
---
+++ src/lib/components/nest/TablePrimitive.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import Primitive from "./Primitive.svelte";
+	import type { PrimitiveType } from "./Primitive.svelte";
+
+	
+	export let value: PrimitiveType[] = [];
+	export let showNumbers = true;
+
+</script>
+
+<table class="table-border-base w-fit rounded border-zinc-300 dark:!border-zinc-600 [&_*]:!border-inherit">
+	<thead>
+		{#if showNumbers}
+			<th class="text-center px-2">Index</th>
+		{/if}
+		<th class="text-center">Value</th>
+	</thead>
+	<tbody>
+		{#each value as v, index}
+			<tr>
+				{#if showNumbers}
+					<td class="px-1 text-center">
+						{index}
+					</td>
+				{/if}
+				<td><Primitive value={v}></Primitive></td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
