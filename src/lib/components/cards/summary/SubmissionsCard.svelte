fa2d4672d893ec9f181724a68b65d6affc410329
sickl8 <sicklevy@gmail.com>
2024-08-20 23:15:37 +0100

    fixed submissions and some of indicators, still needs the severity style changed

--- src/lib/components/cards/summary/SubmissionsCard.svelte
+++ src/lib/components/cards/summary/SubmissionsCard.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import type { Saferwall } from '$lib/types';
 	import { timestampToFormattedDate } from '$lib/utils';
+	import { countryNames } from '$lib/utils/contryNames';
 	import Card from '../../Card.svelte';
 	import ButtonShowMore from '../../form/ButtonShowMore.svelte';
+	import "/node_modules/flag-icons/css/flag-icons.min.css";
 
 	const maxRecords = 10;
 	export let submissions: Saferwall.Submission[] = [];
@@ -25,13 +27,18 @@
 				<th class="font-semibold">Source</th>
 				<th class="font-semibold">Country</th>
 			</thead>
-			<tbody>
+			<tbody class="[&_td]:whitespace-nowrap">
 				{#each submissions as submission, i}
 					<tr class:hide={!expanded && i > maxRecords}>
 						<td><time>{timestampToFormattedDate(submission.timestamp)}</time></td>
 						<td>{submission.filename}</td>
-						<td class="capitalize">{submission.src}</td>
-						<td>{submission.country}</td>
+						<td class="capitalize w-full">{submission.src}</td>
+						<td>
+							<div class="flex gap-2">
+								<span class="fi fi-{submission.country.toLowerCase()}"></span>
+								<span>{countryNames[submission.country.toLowerCase()]}</span>
+							</div>
+						</td>
 					</tr>
 				{/each}
 			</tbody>
