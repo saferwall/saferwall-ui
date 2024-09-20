1140ea743139280d97ed297ca7556f82ac0f2542
sickl8 <sicklevy@gmail.com>
2024-09-20 19:43:44 +0100

    added query, need to add ui logic later

--- src/routes/(file)/files/[hash]/strings/+page.ts
+++ src/routes/(file)/files/[hash]/strings/+page.ts
@@ -7,11 +7,16 @@ export const load = (async ({ params, url, parent }) => {
 
 	const page = Math.abs(parseInt(url.searchParams.get('page')!) || 1);
 	const perPage = Math.abs(parseInt(url.searchParams.get('per_page')!) || 10);
+	const query = url.searchParams.get('q');
 
 	const urlParams = new URLSearchParams({
 		page: page.toString(),
 		per_page: perPage.toString()
 	});
+
+	if (query !== null) {
+		urlParams.append("q", query);
+	}
 
 	const pagination = await client.request<Saferwall.Pagination<{ encoding: string, value: string }>>(
 		`files/${params.hash}/strings?${urlParams.toString()}`
