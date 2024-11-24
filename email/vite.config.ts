8fe5b930941ea85ecdbecc999cb4fc38c90daa7f
sickl8 <sicklevy@gmail.com>
2024-11-24 17:22:43 +0100

    email now renders correctly, only thing left is to move base64 images to other parts in the multipart email request and replace them with CID references

--- email/vite.config.ts
+++ email/vite.config.ts
@@ -1,17 +1,19 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
-import inlineSource from "vite-plugin-inline-source";
 import { ViteEjsPlugin } from "vite-plugin-ejs";
 import { Plugin } from 'vite';
 import { OutputBundle, OutputAsset, OutputOptions } from 'rollup';
+import postcssLoadConfig from 'postcss-load-config';
+import customPostcssPlugin from "./src/validate";
+import { Resvg } from "@resvg/resvg-js";
 
 function escapeForRegex(str: string): string {
 	return str.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
 }
 
-function inlineCssPlugin(): Plugin {
+function patchHtmlAndMergeWithCssPlugin(): Plugin {
 	return {
-		name: 'vite-plugin-inline-css',
+		name: 'vite-plugin-email-compat',
 		enforce: 'post',
 		apply: 'build',
 		async generateBundle(_options: OutputOptions, bundle: OutputBundle) {
@@ -21,7 +23,7 @@ function inlineCssPlugin(): Plugin {
 				fileName = chunk.fileName = chunk.fileName.replace("src/", "");
 				// Process only HTML files
 				if (chunk.type === 'asset' && fileName.endsWith('content.html')) {
-					let htmlContent = (chunk as OutputAsset).source as string;
+					let htmlContent = chunk.source as string;
 					
 					// Find CSS files and inline their content
 					const cssAssets = Object.entries(bundle).filter(
@@ -30,10 +32,10 @@ function inlineCssPlugin(): Plugin {
 					);
 
 					for (const [cssFileName, cssAsset] of cssAssets) {
-						const cssContent = (cssAsset as OutputAsset).source as string;
+						const cssString = (cssAsset as OutputAsset).source as string;
 
 						// Create a <style> tag with the CSS content
-						const styleTag = `<style>${cssContent}</style>`;
+						const styleTag = `<style>${cssString}</style>`;
 
 						// Inline the <style> tag into the HTML
 						htmlContent = htmlContent.replace(
@@ -45,8 +47,41 @@ function inlineCssPlugin(): Plugin {
 						toDelete.push(cssFileName)
 					}
 
+					// insert a second head to compensate for common bug in how email providers handle css
+					// we're talking google, microsoft, big names doing stupid and unnecessary things
+					// insert a third for the Inter font for the clients that support it
+					htmlContent = htmlContent.replace(/<html [^>]*>/, `$&<head></head>`);
+					
+					// gmail doesn't support escaped characters in classnames...
+					htmlContent = htmlContent.replace(/class="([^"]*?)"/g, (match, classNames: string) => {
+						const updatedClassNames = classNames
+							.split(' ')
+							.map((cls) => cls.replace(/([!"#$%&'()*+,./:;<=>?@[\\\]^`{|}~])/g, '__'))
+							.join(' ');
+						return `class="${updatedClassNames}"`;
+					});
+					
+					// turn svgs into base64 pngs
+					htmlContent = htmlContent.replace(/(?:"data:image\/svg\+xml,)([^"]*)(?:"\s+class="(?:size|w)-)([0-9]+)(?:)/g, (match, svgEncoded, size) => {
+						const multiplier = 2;
+						let widthInPx = size ? parseInt(size) * 4 * multiplier : 512;
+						let svgString = decodeURIComponent(svgEncoded);
+						let resvg = new Resvg(svgString, {
+							fitTo: {
+								mode: "width",
+								value: widthInPx,
+							},
+							dpi: 300,
+						});
+						let pngData = resvg.render();
+						let pngBuffer = pngData.asPng();
+						let pngBase64url = "data:image/png;base64," + pngBuffer.toString("base64");
+						// console.log({svgString, widthInPx, pngBase64url});
+						return match.replace(/"data:[^"]*"/, `"${pngBase64url}"`);
+					})
+
 					// Update the HTML file content in the bundle
-					(chunk as OutputAsset).source = htmlContent;
+					chunk.source = htmlContent;
 				} else if (chunk.type === 'asset' && fileName.endsWith('text.html')) {
 					bundle[chunk.fileName = fileName.replace("text.html", "content.txt")] = chunk;
 					delete bundle[fileName];
@@ -76,24 +111,37 @@ const ejsVars = {
 	},
 	get verify_account_link() {
 		return `https://saferwall.com/auth/confirm/verify?token={{ .Token }}&guid={{ .Guid }}&id={{ .Email }}`;
-	}
+	},
+	get update_email_link() {
+		return `https://saferwall.com/auth/confirm/verify?token={{ .Token }}&guid={{ .Guid }}&id={{ .Email }}`;
+	},
 }
 
-export default defineConfig({
-	plugins: [
-		ViteEjsPlugin(ejsVars),
-		inlineSource({}),
-		inlineCssPlugin(),
-	],
-	build: {
-		rollupOptions: {
-			input: Object.fromEntries(inputs.map(input => {
-				return [
-					[input + ".html", resolve(__dirname, `src/${input}/content.html`)],
-					[input + ".txt", resolve(__dirname, `src/${input}/text.html`)],
-				];
-			}).flat()),
-
+export default defineConfig(async ({command}) => {
+	return {
+		plugins: [
+			ViteEjsPlugin(ejsVars),
+			patchHtmlAndMergeWithCssPlugin(),
+		],
+		build: {
+			rollupOptions: {
+				input: Object.fromEntries(inputs.map(input => {
+					return [
+						[input + ".html", resolve(__dirname, `src/${input}/content.html`)],
+						[input + ".txt", resolve(__dirname, `src/${input}/text.html`)],
+					];
+				}).flat()),
+	
+			},
 		},
-	},
+		css: {
+			postcss: {
+				plugins: [
+					...(await postcssLoadConfig()).plugins,
+					...(command === "build" ? [ customPostcssPlugin ] : [])
+				],
+			}
+		},
+		clearScreen: false,
+	}
 });
