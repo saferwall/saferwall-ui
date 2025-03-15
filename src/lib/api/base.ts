950a2e27d4ece90728b54374e5e8b396e2912bcd
sickl8 <sicklevy@gmail.com>
2025-03-15 04:53:40 +0000

    wired up bulk download

--- src/lib/api/base.ts
+++ src/lib/api/base.ts
@@ -13,13 +13,14 @@
  */
 
 
+import { env } from '$env/dynamic/public';
 import type { Configuration } from './configuration';
 // Some imports not used depending on template conditions
 // @ts-ignore
 import type { AxiosPromise, AxiosInstance, RawAxiosRequestConfig } from 'axios';
 import globalAxios from 'axios';
 
-export const BASE_PATH = "https://api.saferwall.com/v1".replace(/\/+$/, "");
+export const BASE_PATH = env.PUBLIC_API_URL.replace(/\/+$/, "");
 
 /**
  *
---
+++ src/routes/api/files/download/+server.ts
@@ -0,0 +1,38 @@
+import { SaferwallClient } from '$lib/clients/saferwall.js';
+import { env } from '$env/dynamic/public';
+import { SESSION_KEY } from '$lib/config';
+import { redirect } from '@sveltejs/kit';
+
+export async function POST({ fetch, cookies, request }) {
+	const cookieString = await cookies.get(SESSION_KEY);
+	if (cookieString) {
+		try {
+			const client = new SaferwallClient(JSON.parse(cookieString), fetch);
+			const formData = await request.formData();
+			let data = formData.get("hashes");
+			if (typeof data !== "string") {
+				redirect(302, "/");
+			}
+			console.log({data, body: JSON.stringify({ hashes: JSON.parse(data) }),});
+			let res = await fetch(
+				`${env.PUBLIC_API_URL}files/bulk-download/`,
+				{
+					headers: {
+						"Authorization": client.authorization!,
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({ hashes: JSON.parse(data) }),
+					method: "POST",
+				},
+			)
+			if (res.status === 401) {
+				redirect(302, "/auth/login");
+			}
+			return res;
+		} catch (e) {
+			console.log(e);
+			// redirect(302, "/auth/logout");
+		}
+	}
+	redirect(302, "/auth/login");
+};
